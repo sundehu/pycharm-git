1b1e4cec6035c6c69fa791af3a7e9cc1b315bb7c
sundehu <965001257@qq.com>
2022-03-25 15:18:29 +0800

    first test

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print('sundehu')
     print('git pytest')
+    print('dh_git')
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
